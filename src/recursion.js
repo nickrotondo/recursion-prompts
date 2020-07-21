2647be8c02d2b2b5261f9836f68d289746b8c369
nickrotondo <nickrotondo@gmail.com>
2020-07-21 15:56:54 -0700

    Complete range function

--- src/recursion.js
+++ src/recursion.js
@@ -67,6 +67,17 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  result = [];
+  if (x === y - 1 || x === y + 1 || x === y) {
+    return result;
+  }
+  if (x < y) {
+    result.push(x + 1);
+    return result.concat(range(x + 1, y));
+  } else {
+    result.push(x - 1);
+    return result.concat(range(x - 1, y));
+  }
 };
 
 // 7. Compute the exponent of a number.
